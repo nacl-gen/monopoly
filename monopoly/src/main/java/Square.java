598190cc764481af35a09e8e57f290d94f939df1
Christoph Rueff <christoph.rueff@gmail.com>
2019-04-26 13:01:09 +0200

    Add number to Square, with getNumber method

--- monopoly/src/main/java/Square.java
+++ monopoly/src/main/java/Square.java
@@ -1,12 +1,17 @@
 public class Square {
     private String name;
+    private int number;
 
-    Square(String name){
-        this.name =name;
+    Square(String name, int number){
+        this.name = name;
+        this.number = number;
     }
 
     public String getName() {
         return name;
+    }
+    public int getNumber() {
+        return number;
     }
 }
 
